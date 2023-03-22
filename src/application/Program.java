c8c004e9c588bc31c727b866097b6ca275baa782
Michel Morais <valentimmichel@gmail.com>
2023-03-22 20:42:33 -0300

    Solution with comparable interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,22 +7,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import entities.Employee;
+
 public class Program {
 	public static void main(String[] args) {
 
-		List<String> list = new ArrayList<>();
+		List<Employee> list = new ArrayList<>();
 		String path = "C:\\temp\\in.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String employee = br.readLine();
+			while (employee != null) {
+				String[] fields = employee.split(",");
+				list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+				employee = br.readLine();
 			}
 			Collections.sort(list);
-			for (String s : list) {
-				System.out.println(s);
+			for (Employee emp : list) {
+				System.out.println(emp.getName() + ", " + String.format("%.2f", emp.getSalary()));
 			}
 
 		} catch (IOException e) {
